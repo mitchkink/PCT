111df021cc834a629983afea257bfc50c0393d12
mitchkink <mitchkinkeade@sbcglobal.net>
2014-03-15 01:29:10 -0500

    Update movie.js

--- js/frontend/models/movie.js
+++ js/frontend/models/movie.js
@@ -3,28 +3,36 @@ App.Model.Movie = Backbone.Model.extend({
     buildBasicView: function () {
     
       var model = this;
-    
-      model.set('loaded',   false);
-      model.set('image',    model.get('coverImage'));
-      model.set('bigImage', model.get('coverImage'));
-      model.set('title',    model.get('title'));
-      model.set('synopsis', '');
-      model.set('voteAverage', null);
-      model.set('runtime', null);
+
+      // This is mostly used for reporting
+      model.set('slug',       model.get('title').toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'_') +'.'+ model.get('imdb').slice(2) );
+      model.set('niceTitle',  model.get('title') +' ('+model.get('year')+')' );
 
       model.view = new App.View.MovieListItem({
           model: model
       });
+      
+      model.trigger('rottenloaded');
     },
 
+    getShortTitle: function() {
+      if (this.get('title').length > 19) {
+        return this.get('title').substring(0, 13) + "...";
+      }
+
+      return this.get('title');
+    },
+
+    // DEPRECATED
     setRottenInfo: function () {
         var model = this;
 
         App.findMovieInfo(model.get('imdb'), function (data) {
             
-            model.set('loaded',   true);
+            model.set('infoLoaded', true);
             model.set('image',    data.image);
             model.set('bigImage', data.image);
+            model.set('backdrop', data.backdrop);
             model.set('title',    data.title);
             model.set('synopsis', data.overview);
             model.set('voteAverage', data.voteAverage);
@@ -38,6 +46,7 @@ App.Model.Movie = Backbone.Model.extend({
         });
     },
 
+    // DEPRECATED
     setSubtitles: function () {
         var model = this;
 
@@ -45,42 +54,49 @@ App.Model.Movie = Backbone.Model.extend({
             imdb: model.get('imdb'),
             title: model.get('title')
         }, function (info) {
+            model.set('subbtitlesLoaded', true);
             model.set('subtitles', info);
+
+            model.trigger('rottenloaded');
         });
     },
 
     initialize: function () {
-        // Movie Health
-        var seeders = this.get('seeders');
-        var leechers = this.get('leechers');
-        var ratio = leechers > 0 ? (seeders / leechers) : seeders;
-
-        if (seeders < 100) {
-            this.set('health', 'bad');
-        }
-        else if (seeders > 100 && seeders < 200) {
-            if( ratio > 5 ) {
-                this.set('health', 'good');
-            } else if( ratio > 3 ) {
-                this.set('health', 'medium');
-            } else {
-                this.set('health', 'bad');
-            }
-        }
-        else if (seeders > 200) {
-            if( ratio > 5 ) {
-                this.set('health', 'excellent');
-            } else if( ratio > 3 ) {
-                this.set('health', 'good');
-            } else if( ratio > 2 ) {
-                this.set('health', 'medium');
-            } else {
-                this.set('health', 'bad');
-            }
-        }
-
         this.buildBasicView();
-        this.setRottenInfo();
-        this.setSubtitles();
+        //this.setRottenInfo();
+        //this.setSubtitles();
+        this.calculateTorrentHealth();
+    },
+
+    calculateTorrentHealth: function () {
+      // Calculates the "health" of the torrent (how easy it is to stream)
+      var seeders = this.get('seeders');
+      var leechers = this.get('leechers');
+      var ratio = leechers > 0 ? (seeders / leechers) : seeders;
+
+      if (seeders < 100) {
+        this.set('health', 'bad');
+      }
+      else if (seeders > 100 && seeders < 200) {
+        if( ratio > 5 ) {
+          this.set('health', 'good');
+        } else if( ratio > 3 ) {
+          this.set('health', 'medium');
+        } else {
+          this.set('health', 'bad');
+        }
+      }
+      else if (seeders > 200) {
+        if( ratio > 5 ) {
+          this.set('health', 'excellent');
+        } else if( ratio > 3 ) {
+          this.set('health', 'good');
+        } else if( ratio > 2 ) {
+          this.set('health', 'medium');
+        } else {
+          this.set('health', 'bad');
+        }
+      }
     }
+
 });
